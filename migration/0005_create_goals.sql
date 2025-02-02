d0d0e7d6f4ebd38f091199bac2185da50be03c8d
Viktoria Serikova <viktoria.serikova2016@yandex.ru>
2025-02-03 01:00:13 +0300

    add goals migration

---
+++ migration/0005_create_goals.sql
@@ -0,0 +1,15 @@
+CREATE TABLE IF NOT EXISTS goals (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    user_id UUID NOT NULL,
+    title VARCHAR(255) NOT NULL,
+    description TEXT,
+    status VARCHAR(50) NOT NULL DEFAULT 'in-progress',
+    estimated_time INT NOT NULL DEFAULT 0,
+    created_at TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT NOW(),
+
+    CONSTRAINT fk_goal_user
+    FOREIGN KEY (user_id)
+    REFERENCES users(id)
+    ON DELETE CASCADE
+);
