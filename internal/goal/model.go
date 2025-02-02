900e46febf357613adb6033c4a638fbeecdcd265
Viktoria Serikova <viktoria.serikova2016@yandex.ru>
2025-02-03 00:34:45 +0300

    Add Phase and Task models

--- internal/goal/model.go
+++ internal/goal/model.go
@@ -12,3 +12,26 @@ type Goal struct {
 	CreatedAt     time.Time `json:"created_at"`
 	UpdatedAt     time.Time `json:"updated_at"`
 }
+
+type Phase struct {
+	ID            int64     `json:"id"`
+	GoalId        int64     `json:"goalId"`
+	Title         string    `json:"title"`
+	Description   string    `json:"description"`
+	Status        string    `json:"status"`
+	EstimatedTime int64     `json:"estimated_time"`
+	CreatedAt     time.Time `json:"created_at"`
+	UpdatedAt     time.Time `json:"updated_at"`
+}
+
+type Task struct {
+	ID            int64     `json:"id"`
+	GoalId        int64     `json:"goalId"`
+	PhaseId       int64     `json:"phase_id"`
+	Title         string    `json:"title"`
+	Description   string    `json:"description"`
+	Status        string    `json:"status"`
+	EstimatedTime int64     `json:"estimated_time"`
+	CreatedAt     time.Time `json:"created_at"`
+	UpdatedAt     time.Time `json:"updated_at"`
+}
